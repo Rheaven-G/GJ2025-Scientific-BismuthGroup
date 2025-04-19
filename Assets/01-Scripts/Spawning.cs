d2ff1b44db955023bf944f05114c792706397678
sopastic <166405522+sopastic@users.noreply.github.com>
2025-04-19 15:45:03 -0400

    NPC spawner base

--- Assets/01-Scripts/Spawning.cs
+++ Assets/01-Scripts/Spawning.cs
@@ -7,16 +7,34 @@ public class Spawning : MonoBehaviour
     public GameObject prefabToSpawn;
     public Transform spawnL;
     public Transform spawnR;
+    public GameObject[] positions;
+    public GameObject newObjectL;
+
+
+    public int spawnNumber = 2;
+    private int randomNumber = 0;
 
 
     void Start()
     {
-        GameObject newObjectL = Instantiate<GameObject>(prefabToSpawn, spawnL.position, Quaternion.identity);
-        GameObject newObjectR = Instantiate<GameObject>(prefabToSpawn, spawnR.position, Quaternion.identity);
+        GenerateNumber();
+        newObjectL = Instantiate<GameObject>(prefabToSpawn, spawnL.position, Quaternion.identity);
+        newObjectL.GetComponent<NPCMovement>().target = positions[randomNumber];
+        //GameObject newObjectR = Instantiate<GameObject>(prefabToSpawn, spawnR.position, Quaternion.identity);
     }
 
     void Update()
     {
-        
     }
+
+    void GenerateNumber()
+    {
+        randomNumber = Random.Range(0, 2);
+    }
+
+    void Spawn()
+    {
+
+    }
+
 }
