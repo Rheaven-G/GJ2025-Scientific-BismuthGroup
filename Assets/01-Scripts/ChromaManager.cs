c1540875e0efb20cb9adddf3c5f0ccde1c3b8800
V LABROUSSE <vallab.master@proton.me>
2025-04-20 15:32:17 -0400

    now

--- Assets/01-Scripts/ChromaManager.cs
+++ Assets/01-Scripts/ChromaManager.cs
@@ -2,27 +2,32 @@ using UnityEngine;
 
 public class ChromaManager : MonoBehaviour
 {
+    public Camera Camera;
     public Material fullscreenShader;
+
+    public Plant redPlant;
+    public Plant greenPlant;
+    public Plant bluePlant;
+
+    void Awake()
+    {
+        Camera = FindFirstObjectByType<Camera>().GetComponent<Camera>();
+    }
     
-    public GameObject redPlantCoordinate;
-    public GameObject greenPlantCoordinate;
-    public GameObject bluePlantCoordinate;
-
-    public Plant redPlantIntensity;
-    public Plant greenPlantIntensity;
-    public Plant bluePlantIntensity;
-
 
     void Update()
     {
-        fullscreenShader.SetVector("_1_Coordinates", redPlantCoordinate.transform.position);
-        fullscreenShader.SetVector("_2_Coordinates", greenPlantCoordinate.transform.position);
-        fullscreenShader.SetVector("_2_Coordinates", bluePlantCoordinate.transform.position);
+        // Calculus
+        
+        
+        // Positionning
+        fullscreenShader.SetVector("_1_Coordinates", redPlant.transform.position);
+        fullscreenShader.SetVector("_2_Coordinates", greenPlant.transform.position);
+        fullscreenShader.SetVector("_2_Coordinates", bluePlant.transform.position);
      
-        fullscreenShader.SetVector("_1_Intensity", redPlantIntensity.s);
-        fullscreenShader.SetVector("_2_Intensity", redPlantIntensity.s);
-        fullscreenShader.SetVector("_3_Intensity", redPlantIntensity.s);
-        
-        
+        // Intensity levels
+        fullscreenShader.SetFloat("_1_Intensity", redPlant.GetPlantLevel() * 1);
+        fullscreenShader.SetFloat("_2_Intensity", redPlant.GetPlantLevel() * 1);
+        fullscreenShader.SetFloat("_3_Intensity", redPlant.GetPlantLevel() * 1);
     }
 }
